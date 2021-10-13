1f012e92d2753510ca7affc5809bcf103d5b074b
dima-starkov <62872531+dima-starkov@users.noreply.github.com>
2021-10-13 20:31:37 +0300

    postVC layout

--- Instagramm app/Controllers/Core Tabs/HomeViewController.swift
+++ Instagramm app/Controllers/Core Tabs/HomeViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 import FirebaseAuth
 
+struct HomeFeedRenderViewModel {
+    let header: PostRenderViewModel
+    let post: PostRenderViewModel
+    let action: PostRenderViewModel
+    let comments: PostRenderViewModel
+}
 
 class HomeViewController: UIViewController {
     
+    private var feedRenderModels = [HomeFeedRenderViewModel]()
+    
     private let tableView: UITableView = {
         let table = UITableView()
-        table.register(IGFeedPostTableViewCell.self, forCellReuseIdentifier: IGFeedPostTableViewCell.identifier)
+        table.register(IGFeedPostTableViewCell.self,
+                       forCellReuseIdentifier: IGFeedPostTableViewCell.identifier)
+        table.register(IGFeedPostHeaderTableViewCell.self,
+                       forCellReuseIdentifier: IGFeedPostHeaderTableViewCell.identifier)
+        table.register(IGFeedPostActionsTableViewCell.self,
+                       forCellReuseIdentifier: IGFeedPostActionsTableViewCell.identifier)
+        table.register(IGFeedPostGeneralTableViewCell.self,
+                       forCellReuseIdentifier: IGFeedPostGeneralTableViewCell.identifier)
+        
         
         return table
     }()
