18ea1945f21fd30703eef9bbcb9a95e69c173d34
dima-starkov <62872531+dima-starkov@users.noreply.github.com>
2021-10-16 16:50:45 +0300

    feed controller

--- Instagramm app/Controllers/Core Tabs/HomeViewController.swift
+++ Instagramm app/Controllers/Core Tabs/HomeViewController.swift
@@ -67,17 +67,88 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController: UITableViewDelegate,UITableViewDataSource {
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        let x = section
+        let model: HomeFeedRenderViewModel
+        if x == 0 {
+        model = feedRenderModels[0]
+        } else {
+        let position = x % 4 == 0 ? x/4 : ((x - (x % 4)) / 4)
+        model = feedRenderModels[position]
+        }
+        
+        let subSection = x % 4
+        
+        switch subSection {
+        case 0: return 1
+        case 1: return 1
+        case 2: return 1
+        case 3:
+            let commentsModel = model.comments
+            switch commentsModel.renderType {
+            case .comments(let comments): return comments.count > 2 ? 2: comments.count
+            @unknown default: fatalError("Invalid case")
+            }
+        default: fatalError()
+        }
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return feedRenderModels.count * 4
+    }
+    
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostTableViewCell.identifier,for: indexPath) as! IGFeedPostTableViewCell
+        let x = indexPath.section
         
-        return cell
+        var model: HomeFeedRenderViewModel
+        
+        if x == 0 {
+        model = feedRenderModels[0]
+        } else {
+        let position = x % 4 == 0 ? x/4 : ((x - (x % 4))/4)
+        model = feedRenderModels[position]
+        }
+        
+        switch model.post.renderType {
+        case .actions(let actions):
+            let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostActionsTableViewCell.identifier,
+                                                     for: indexPath) as! IGFeedPostActionsTableViewCell
+        
+            return cell
+            
+        case .header(let user):
+            let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostHeaderTableViewCell.identifier,
+                                                     for: indexPath) as! IGFeedPostHeaderTableViewCell
+      
+            return cell
+            
+        case .primaryContent(let post):
+            let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostTableViewCell.identifier,
+                                                     for: indexPath) as! IGFeedPostTableViewCell
+           
+            return cell
+            
+        case .comments(let comments):
+            let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostGeneralTableViewCell.identifier,
+                                                     for: indexPath) as! IGFeedPostGeneralTableViewCell
+      
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let model = feedRenderModels[indexPath.section]
+        switch model.post.renderType {
+        case .actions(_): return 60
+        case .header(_): return 70
+        case .primaryContent(_): return tableView.widht
+        case .comments(_): return 50
+        }
     }
 }
